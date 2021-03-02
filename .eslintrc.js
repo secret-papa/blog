d30043362ba5021e7ed16f889ab96ef2a0767186
seungha0802 <seungha0802@gmail.com>
2021-03-03 08:20:51 +0900

    chore: jsx 대신 js로 파일 확장자 변경

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,5 +5,5 @@ module.exports = {
   },
   extends: ["airbnb", "prettier", "prettier/react", "plugin:react-hooks/recommended"],
   plugins: ["react", "prettier", "react-hooks"],
-  rules: { "react/prop-types": 0 },
+  rules: { "react/prop-types": 0, "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }] },
 };
