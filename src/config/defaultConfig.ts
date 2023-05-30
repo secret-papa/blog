ada771bfffb0fb07b321b6cf765bd8e1e6c492a6
secret-papa <seungha0802@gmail.com>
2023-05-30 19:46:41 +0900

    chore: set confiture

--- src/config/defaultConfig.ts
+++ src/config/defaultConfig.ts
@@ -3,18 +3,18 @@ import { SiteConfig } from "./types";
 const config: SiteConfig = {
   // Website configuration
   website: {
-    title: "Gatsby Advanced Starter", // Homepage title
-    titleShort: "Advanced Blog", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation
-    name: "Gatsby Advanced Starter", // Website name used for homescreen (PWA) and SEO
-    description: "A GatsbyJS starter equipped with advanced features.", // Website description used for RSS feeds/meta description tag
-    language: "en", // Sets the global HTML lang attribute
+    title: "Secret Blog", // Homepage title
+    titleShort: "S-Blog", // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation
+    name: "Development Secret Blog", // Website name used for homescreen (PWA) and SEO
+    description: "Secret Blog for me", // Website description used for RSS feeds/meta description tag
+    language: "ko", // Sets the global HTML lang attribute
     logoUrl: "/logos/logo-1024.png", // Logo used for SEO
-    url: "https://example.com", // Domain of your website without the pathPrefix
+    url: "https://secret-blog.netlify.app/", // Domain of your website without the pathPrefix
     rss: "/rss.xml", // Path to the RSS file
     rssTitle: "Gatsby Advanced Starter RSS Feed", // Title of the RSS feed
-    copyright: "© Copyright 2021", // Copyright string for the footer of the website and RSS feed.
+    copyright: "© Copyright 2023", // Copyright string for the footer of the website and RSS feed.
     themeColor: "#D83850", // Used for setting manifest and progress theme colors.
-    backgroundColor: "#F7F7F7", // Used for setting manifest background color.
+    backgroundColor: "#FFFFFF", // Used for setting manifest background color.
   },
 
   // User configuration
