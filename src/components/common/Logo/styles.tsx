a720b5572d606987cfe055ea3de82e3bf244eff8
secret-papa <seungha0802@gmail.com>
2023-05-31 12:45:29 +0900

    feat: 게시글 페이지 스타일 적용
    - header를 제작하여 루트 페이지로 이동할 수 있도록 변경
    - 목차를 작성한 게시글도 있으나, 목차를 눌러도 이동하지 않아서 삭제함
      - 이후 #38을 통해서 목차를 제공할 예정
    - gatsby.browser, gatsby.ssr에서 wrapRootElement를 export하면 빌드 시 gatsby-plugins-mdx module을 찾을 수 없다는 에러 발생
      - 우선 Provider들을 wrapPageElement에서 render하도록 처리
    
    refs #34

---
+++ src/components/common/Logo/styles.tsx
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+// eslint-disable-next-line import/prefer-default-export
+export const Text = styled.span`
+  font-size: 1.4rem;
+  color: #2e4f4f;
+`;
---
+++ src/templates/post/styles.tsx
@@ -0,0 +1,87 @@
+import styled, { css } from "styled-components";
+import { Link } from "gatsby";
+
+export const Root = styled.div`
+  margin-bottom: 3rem;
+`;
+
+export const Title = styled.h1`
+  font-size: 3rem;
+  font-weight: bold;
+  color: var(--color-gray-900);
+`;
+
+export const Body = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-top: 5.5rem;
+`;
+
+export const Article = styled.article`
+  width: 768px;
+`;
+
+export const CoverImageWrapper = styled.div`
+  margin-top: 2rem;
+`;
+
+export const CoverImage = styled.img`
+  display: block;
+  width: 100%;
+`;
+
+export const MarkdownWrapper = styled.div`
+  margin: 5rem 0 6rem;
+`;
+
+export const MarkdownBody = styled.div`
+  & ol,
+  ul {
+    list-style: decimal;
+  }
+`;
+
+export const NavigationButtonWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  gap: 1rem;
+`;
+
+const leftDirectionCss = css`
+  text-align: left;
+`;
+const rightDirectionCss = css`
+  text-align: right;
+`;
+
+type Align = "left" | "right";
+
+export const NavigationButton = styled(Link)<{ align?: Align }>`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 0.5rem;
+  width: 100%;
+  height: 4rem;
+  background-color: #f8f9fa;
+  padding-left: 1rem;
+  padding-right: 1rem;
+  box-shadow: rgba(0, 0, 0, 0.06) 0px 0px 4px 0px;
+  border: none;
+  cursor: pointer;
+  ${props => (props.align === "right" ? rightDirectionCss : leftDirectionCss)}
+`;
+
+export const NavigationButtonCaption = styled.span`
+  font-size: 0.75rem;
+  color: var(--color-grey-600);
+`;
+
+export const NavigationButtonTitle = styled.h3`
+  font-size: 1.125rem;
+  font-weight: bold;
+  color: var(--color-grey-700);
+`;
